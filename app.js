c6e8f6d2cff7c5b2487dac9ed78553657a2dd81c
ravi <ravi.20930@gmail.com>
2023-05-22 16:50:51 +0530

    updated a basic jenkins script wirhout docker

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-    res.send('this is a a test for checking github docker auto builds on ec2...........................')
+    res.send('this is a a test for checking github docker auto  on ec2...........................')
 })
 app.listen(3000, () => {
     console.log("running on 3000");
