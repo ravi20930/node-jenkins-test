e53c2ba128952f19b19d86e9d8f33c9c9e971671
ravi <ravi.20930@gmail.com>
2023-05-22 17:36:18 +0530

    jenkins

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-    res.send('this is a a test for checking github docker auto  on ec2, finally not working!!!!!!!!!!!');
+    res.send('this is a a test for checking github docker auto  on ec2, finally notworking!!!!!!!!!!!');
 })
 app.listen(3000, () => {
     console.log("running on 3000");
