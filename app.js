cce19b1a172983af546cdbc42b2582b39ae03ea0
ravi <ravi.20930@gmail.com>
2023-05-22 17:53:16 +0530

    finally wokring

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-    res.send('this is a a test for checking github docker auto  on ec2, finally working!!!!!!!!!!!');
+    res.send('this is a a test for checking github docker auto  on ec2, finally working!!!!!!!!!!!, final check');
 })
 app.listen(3000, () => {
     console.log("running on 3000");
